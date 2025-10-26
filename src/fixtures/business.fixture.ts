13eaab78b804f8e34b053ebd686f936277c0b3de
Oleg S. <kindovso@gmail.com>
2025-10-26 23:53:44 +0100

    refactor(tests): Abstract test setup into fixtures
    Implements a fixture-based setup to clean up tests:
    - Creates `pages.fixture.ts` to auto-instantiate all Page Objects.
    - Creates `business.fixture.ts` to provide a `loginAsAdmin` fixture.
    - Updates `loginAsAdmin` to use the `LoginPage` PO for cleaner logic.
    - Refactors `add-new-product.spec.ts` to consume the new fixtures, removing boilerplate setup and login code.

--- src/fixtures/business.fixture.ts
+++ src/fixtures/business.fixture.ts
@@ -1,19 +1,23 @@
 import { test as base, expect } from "fixtures/pages.fixture";
 import { credentials, SALES_PORTAL_URL } from "config/env";
+import { LoginPage } from "ui/pages/login.page";
 
 export const test = base.extend<{
   loginAsAdmin: () => Promise<void>;
 }>({
-  loginAsAdmin: async ({ page, homePage }, use) => {
+  loginAsAdmin: async ({ loginPage, homePage }, use) => {
     await use(async () => {
-      const emailInput = page.locator("#emailinput");
-      const passwordInput = page.locator("#passwordinput");
-      const loginButton = page.locator("button[type='submit']");
+      // const emailInput = page.locator("#emailinput");
+      // const passwordInput = page.locator("#passwordinput");
+      // const loginButton = page.locator("button[type='submit']");
 
-      await page.goto(SALES_PORTAL_URL);
-      await emailInput.fill(credentials.username);
-      await passwordInput.fill(credentials.password);
-      await loginButton.click();
+      // await page.goto(SALES_PORTAL_URL);
+      // await emailInput.fill(credentials.username);
+      // await passwordInput.fill(credentials.password);
+      // await loginButton.click();
+      await loginPage.open();
+      await loginPage.fillCredentials(credentials);
+      await loginPage.clickLogin();
 
       await homePage.waitForOpened();
     });
